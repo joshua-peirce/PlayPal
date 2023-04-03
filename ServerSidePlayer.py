67e9ec01fcbbfef5a7211cc7df1ef16c9af42ac8
joshuapeirce <peirce.jo@northeastern.edu>
2023-04-03 11:56:58 -0400

    Start of Monday (2)

---
+++ ServerSidePlayer.py
@@ -0,0 +1,23 @@
+class ServerSidePlayer:
+    def __init__(self, rc, ps, id):
+        print("Making server side player", id)
+        self.ps=ps
+        self.rc=rc
+        self.id=id
+
+    def play(self, board):
+        self.rc.publish("game", "a" + self.id + str(board.get_seed()))
+        while True:
+            for message in self.ps.listen():
+                if message["type"] == "message":
+                    data = str(message["data"])[2:-1]
+                    if data[0] == "b":
+                        move = int(data[1:])
+                        if move in board.get_empty_cells_integer():
+                            return board.convert_integer_to_tuple(move)
+                    
+    def win(self, hist):
+        pass
+
+    def lose(self, hist):
+        pass

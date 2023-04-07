9e56f0705cb973504bc1e74ad49518363a61b63d
joshuapeirce <peirce.jo@northeastern.edu>
2023-04-06 22:25:37 -0400

    Made fake game log data

---
+++ game.py
@@ -0,0 +1,62 @@
+"""
+Author: Josh Peirce
+Date: 3/12/23
+
+This class operates the game. It communicates with the players when it is their
+it is their turn and tells them the state of the board. It also runs the
+mainloop  and checks for the game being ower.
+"""
+
+from board import Board
+
+class Game:
+    """Class to hold info and play a game"""
+    
+    def __init__(self, p1, p2):
+        """Make a new game with players p1 and p2"""
+        self.p1 = p1
+        self.p2 = p2
+        self.board = Board(size=3, to_win=3)
+        self.hist = []
+
+    def play(self):
+        """Play a game"""
+        isx = True
+        while not (self.board.check_x_wins() or self.board.check_o_wins() or len(self.board.get_empty_cells()) == 0):
+            move = self.query_move(isx)
+            self.board = self.board.add_piece(*move, isx)
+            self.hist.append(self.board.get_seed())
+            isx = not isx
+        if self.board.check_x_wins():
+            self.p1.win(self.hist)
+            self.p2.lose(self.hist)
+            return "X", self.hist 
+        elif self.board.check_o_wins():
+            self.p1.lose(self.hist)
+            self.p2.win(self.hist)
+            return "O", self.hist 
+        else:
+            self.p1.draw(self.hist)
+            self.p2.draw(self.hist)
+            return "Draw", self.hist 
+
+    def query_move(self, isx):
+        """Get a move from player.
+        Keep asking until the player gives a legal move.
+        This has the potential to infinitely loop - need to be careful here"""
+        opts = self.board.get_empty_cells()
+        move = None
+        while move not in opts:
+            if isx:
+               move = self.p1.play(self.board)
+            else:
+                move = self.p2.play(self.board)
+        return move
+        
+if __name__ == "__main__":
+    import player #perhaps putting imports here is bad practice but idk
+    import ai_player
+    px = player.Player(True)
+    po = ai_player.RandomPlayer(False)
+    game = Game(px, po)
+    game.play()
---
+++ game_data_generator.py
@@ -0,0 +1,44 @@
+import ai_player
+import game
+import pandas as pd
+import random
+
+NUM_GAMES = 100000
+NUM_PLAYERS = 1000
+
+def play_random_game(p1n, p2n):
+    p1 = ai_player.RandomPlayer("X")
+    p2 = ai_player.RandomPlayer("O")
+    G = game.Game(p1, p2)
+    return G.play()
+
+if __name__ == "__main__":
+    df = pd.DataFrame()
+    for i in range(NUM_GAMES):
+        #Make player names
+        player_1_name = "Player " + str(random.randrange(NUM_PLAYERS))
+        player_2_name = "Player " + str(random.randrange(NUM_PLAYERS))
+        while player_1_name == player_2_name:
+            #Not duplicate names
+            player_2_name = "Player " + str(random.randrange(NUM_PLAYERS))
+        #Play the game and get the result
+        winner, hist = play_random_game(player_1_name, player_2_name)
+        #Save the winner's name instead of piece
+        if winner == "X":
+            winner = player_1_name
+        elif winner == "O":
+            winner = player_2_name
+        #Make a df to represent this result
+        this = pd.DataFrame(
+            {"Game_id": i,
+             "P1": player_1_name,
+             "P2": player_2_name,
+             "Winner": winner,
+             "History": str(hist)}, index=pd.Index([i])
+            )
+        this = this.set_index("Game_id")
+        #Add this result to the dataframe
+        df = pd.concat((df, this), axis=0)
+        if i % (NUM_GAMES // 100) == 0:
+            print(i)
+    df.to_csv("game_data.csv")
